258d91bebab6c49fe921e5e04cdf16a767a6dc8b
Obed_Osei <obeds12345@gmail.com>
2020-04-12 16:25:44 -0500

    Added static files

---
+++ static/index.js
@@ -0,0 +1,37 @@
+// function option() {
+//     var element = document.getElementById("option");
+//     if (element.classList.contains("content")) {
+//         element.classList.remove("content");
+//         element.classList.add("mystyle");
+//     } else {
+//         element.classList.remove("mystyle");
+//         element.classList.add("content");
+//     }
+// }
+
+// var today = new Date();
+// var dd = today.getDate();
+// var mm = today.getMonth() + 1; //January is 0!
+
+// var yyyy = today.getFullYear();
+// if (dd < 10) {
+//     dd = '0' + dd;
+// }
+// if (mm < 10) {
+//     mm = '0' + mm;
+// }
+// var today = dd + '/' + mm + '/' + yyyy;
+// document.getElementById('date').value = today;
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
